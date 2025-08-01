92a36bbff7682429b603e05d260e6cf94fabb503
jbeezy <lemonlimelemonlimelemonlimel@gmail.com>
2025-07-31 20:16:10 -0400

    I can't avoid it any longer. It needs to be on github so I can clone it.

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+mod errors;
+mod parsing;
+
+use std::{env, process::exit};
+use errors::ParsingError;
+
+use crate::parsing::{sort_tokens, tokenize_args};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 { println!("No arguments. type \"pixtools help\"."); exit(1)}
+
+    // A URL should always be the 2nd argument.
+    let file_location: String =  parse_url(&args).unwrap_or_else(|error| {
+        println!("ERROR: This is not a valid URL. Dickhead. Make sure it's an image too. also type \"pixtools help\". ({:?})", error);
+        exit(1);
+    });
+
+    // Get each of the arguments, file them into order of operation. This is needed so we don't do things wrong.
+    let tokens: Vec<parsing::TOKEN> = tokenize_args(&args);
+    let order: Vec<parsing::TOKEN> = sort_tokens(&tokens);
+
+    if order.len() < 1 { println!("Did you really just give me a NULL vector? Fuck you. Add some more args next time.") }
+
+    println!("Success! Here is your url: \"{}\" and are your tokens in order: {:?}", file_location, order);
+}
+
+fn parse_url(args: &Vec<String>) -> Result<String, ParsingError> {
+    if args.len() <= 1 {
+        return Err(ParsingError::MissingArguments);
+    }
+
+    if args[1].contains("help") {
+        display_help_message();
+        exit(0);
+    } 
+    
+    else if !args[1].contains("url") {
+        return Err(ParsingError::InvalidArgument);
+    }
+
+    let arg: &String = &args[1];
+    Ok(arg.to_string())
+}
+
+fn display_help_message() {
+    println!("\nPixtools cleans pictures with from weird URLs and saves it to your downloads.");
+    println!("Unfortunately saving them from Discord is not enough. Here's how you do it:\n");
+    println!("pixtools *URL/PATH* *TOKENS*");
+    println!("pixtools https://www.mdxblog.io/images/posts/how-to-use-images/grass-tree-sky.jpg -cl -gif\n");
+    println!("Pixtools commands:
+    -clean -cl  Removes all embeds from an image, results in a new clean file saved to your downloads.
+    -path       Forces Pixtools to use a file path instead of a url (the default) This command works with -clean.
+    -gif        Converts the image to .gif before saving. This command is works with -clean and -path.
+    -png        Converts the image to .png before saving. This command is works with -clean and -path.
+    help        Displays this message. Only if it's the only command though.
+    ");
+    println!("Pixtools only does what you tell it to. If you add no tokens it simply downloads the image from a url.");
+}
---
+++ src/parsing.rs
@@ -0,0 +1,53 @@
+use std::process::exit;
+
+#[derive(PartialEq, Debug)]
+pub enum TOKEN {
+    CLEAN,
+    PATH,
+    GIF,
+    PNG,
+    ERR { wrong_token: String }
+}
+
+// Turns shitty strings into clearly computable tokens
+pub fn tokenize_args(args: &Vec<String>) -> Vec<TOKEN> {
+    let mut tokens: Vec<TOKEN> = args.iter().map(|arg| {
+        if matches!(arg.as_str(), "-clean" | "cl") { TOKEN::CLEAN }
+        else if matches!(arg.as_str(), "-path" | "-p") { TOKEN::PATH }
+        else if matches!(arg.as_str(), "-gif") { TOKEN::GIF }
+        else if matches!(arg.as_str(), "-png") { TOKEN::PNG }
+        else { TOKEN::ERR{ wrong_token: arg.to_owned() } }
+    }).collect();
+
+    tokens.drain(0..2);
+    return tokens
+}
+
+// Order: Path Type (path or url), Mode (clean or not), Extention (png, url, etc).
+pub fn sort_tokens(tokens: &Vec<TOKEN>) -> Vec<TOKEN> {
+    let mut sorted_tokens: Vec<TOKEN> = Vec::new();
+    let mut fucked: bool = false;
+
+    if tokens.contains(&TOKEN::PATH) { sorted_tokens.push(TOKEN::PATH); }
+    if tokens.contains(&TOKEN::CLEAN) { sorted_tokens.push(TOKEN::CLEAN); }
+    if tokens.contains(&TOKEN::GIF) { sorted_tokens.push(TOKEN::GIF); }
+    if tokens.contains(&TOKEN::PNG) { sorted_tokens.push(TOKEN::PNG); }
+
+    if tokens.contains(&TOKEN::GIF) && tokens.contains(&TOKEN::PNG) {
+        println!("ERROR: You can't convert your image into 2 different types. (InvalidArgument)");
+        fucked = true;
+    }
+
+    for t in tokens {
+        if let TOKEN::ERR { wrong_token } = t {
+            println!("ERROR: Unreadable token ({}).", wrong_token);
+            fucked = true;
+        }
+    }
+    if fucked { 
+        println!("You must be retarded. That's ok. Type \"pixtools help\" for help.");
+        exit(1) 
+    }
+
+    return sorted_tokens
+}
